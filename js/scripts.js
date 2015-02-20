f1f9ad488c74f61a24fd90a2d8308752d9c5d3d6
Dan Wright <muscle.crab.13@gmail.com>
2015-02-20 11:31:25 -0800

    add slice sizing functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,41 @@
 var Pizza = {
   getSize: function() {
-    return "Your pizza is " + this.size + " inches in diameter!";
+    return "Your " + this.type + " pizza is " + this.size +
+    " inches in diameter, and we are going to slice it into "
+    + this.sliceNumber() + " pieces.";
+  },
+
+  sliceNumber: function() {
+    return Math.round(((this.size*3.14)/this.sliceSize));
   }
+
 };
+
+
 
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
 
+    inputtedPizzaType = $('input[name="pizza-type"]:checked').val();
+
     var inputtedSize = $("input#size").val();
     var newPizza = Object.create(Pizza);
     newPizza.size = inputtedSize;
+    newPizza.type = inputtedPizzaType;
 
-    $("div#results").text(newPizza.getSize());
+    switch(newPizza.type) {
+      case "pepperoni":
+        newPizza.sliceSize = 5;
+        break;
+
+      case "cheese":
+        newPizza.sliceSize = 3;
+        break;
+    }
+
+    $("div#results").append(newPizza.getSize());
 
   });
+
 });
