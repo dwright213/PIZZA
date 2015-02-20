04868dfaf782529f2d7900cde4a2026f77864bd7
Dan Wright <muscle.crab.13@gmail.com>
2015-02-20 12:14:49 -0800

    complete spec coverage

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,26 @@
 var Pizza = {
-  getSize: function() {
+  getDetails: function() {
     return "Your " + this.type + " pizza is " + this.size +
     " inches in diameter, and we are going to slice it into "
     + this.sliceNumber() + " pieces.";
   },
 
-  sliceNumber: function() {
-    return Math.round(((this.size*3.14)/this.sliceSize));
-  }
+  sliceSize: function() {
+    switch(this.type) {
 
+      case "pepperoni":
+        return 5;
+        break;
+
+      case "cheese":
+        return 3;
+        break;
+    }
+  },
+
+  sliceNumber: function() {
+    return Math.round(((this.size*3.14)/this.sliceSize()));
+  }
 };
 
 
@@ -17,24 +29,14 @@ $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
 
-    inputtedPizzaType = $('input[name="pizza-type"]:checked').val();
-
+    var inputtedPizzaType = $('input[name="pizza-type"]:checked').val();
     var inputtedSize = $("input#size").val();
     var newPizza = Object.create(Pizza);
+
     newPizza.size = inputtedSize;
     newPizza.type = inputtedPizzaType;
 
-    switch(newPizza.type) {
-      case "pepperoni":
-        newPizza.sliceSize = 5;
-        break;
-
-      case "cheese":
-        newPizza.sliceSize = 3;
-        break;
-    }
-
-    $("div#results").append(newPizza.getSize());
+    $("div#results").append(newPizza.getDetails());
 
   });
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,25 @@
-describe('queenAttack', function() {
-  it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other', function() {
-    expect(queenAttack([1, 1], [2, 3])).to.equal(false);
+describe("Pizza", function() {
+  describe("sliceSize()", function() {
+    it("Returns the width of the slices that the user's pizza will be cut into", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.type = "cheese";
+      expect(testPizza.sliceSize()).to.equal(3);
+    });
+  });
+  describe("sliceNumber()", function() {
+    it("Returns the approximate number of slices the user can expect their pizza to be cut into", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.size = 12;
+      testPizza.type = "cheese";
+      expect(testPizza.sliceNumber()).to.equal(13);
+    });
+  });
+  describe("getDetails()", function() {
+    it("Confirms to the user their pizza's size, toppings, and number of slices", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.size = 12;
+      testPizza.type = "cheese";
+      expect(testPizza.getDetails()).to.equal("Your cheese pizza is 12 inches in diameter, and we are going to slice it into 13 pieces.");
+    });
   });
 });
